9c216e622a55abe9e18d99d7c1b31cda94fcc5b4
Garrett G <kendallgee99@gmail.com>
2023-04-25 23:45:54 +0000

    saving sorting exercise

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -1,7 +1,18 @@
-/* global $, sessionStorage */
+         
+          /* global $, sessionStorage */
+$(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()  
 
-$(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
+    
+
+          
+
+          // This Function will be called when the ready button is clicked.
+
+
+
+
+
+
 function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
@@ -10,13 +21,27 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  var KEY = {
+    "LEFTA": 65,
+    "RIGHTD": 68,
+    "LEFT1": 97,
+    "RIGHT3": 99,
+  }
+  var paddle1Xcoordinate = -48;
+  var paddle2Xcoordinate = 338.25;
+  var paddle1Xspeed = 0;
+  var paddle2Xspeed = 0;
+  let interval;
+
   
   // Game Item Objects
 
 
   // one-time setup
-  let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on("keydown", handleKeyDown);             
+  $(document).on("keyup", handleKeyUp);
+   // Game checks if its ready to start
+   $(".start").on('click', handleStartClick);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,15 +52,48 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem()
+    redrawGameItem()
 
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleStartClick() {
+    interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+    $(".start").hide()
+  }
 
+
+  function handleKeyDown(event) {
+    if (event.which === KEY.LEFTA) {
+      paddle1Xspeed = -5;
+    }
+    if (event.which === KEY.RIGHTD) {
+      paddle1Xspeed = 5;
+    }
+    if (event.which === KEY.LEFT1) {
+      paddle2Xspeed = -5;
+    }
+    if (event.which === KEY.RIGHT3) {
+      paddle2Xspeed = 5;
+    }
+  }
+
+  function handleKeyUp(event) {
+    if (event.which === KEY.LEFTA) {
+      paddle1Xspeed = 0;
+    }
+    if (event.which === KEY.RIGHTD) {
+      paddle1Xspeed = 0;
+    }
+    if (event.which === KEY.LEFT1) {
+      paddle2Xspeed = 0;
+    }
+    if (event.which === KEY.RIGHT3) {
+      paddle2Xspeed = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -50,5 +108,21 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
+  function repositionGameItem() {
+    paddle1Xcoordinate += paddle1Xspeed
+    paddle2Xcoordinate += paddle2Xspeed
+   }
+ 
+   function redrawGameItem() {
+    $('.paddle1').css("left", paddle1Xcoordinate)
+    $('.paddle2').css("left", paddle2Xcoordinate)
+  }
 }
+
+
+
+
+
+
+
