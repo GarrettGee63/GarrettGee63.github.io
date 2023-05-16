a5194abf456a3c4609dc62ac2c77b6dd867a2571
Garrett G <kendallgee99@gmail.com>
2023-05-16 22:26:26 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -24,13 +24,16 @@ function runProgram(){
   var KEY = {
     "LEFTA": 65,
     "RIGHTD": 68,
-    "LEFT1": 97,
-    "RIGHT3": 99,
+    "arrowLeft": 37,
+    "arrowRight": 39,
   }
-  var paddle1Xcoordinate = -48;
-  var paddle2Xcoordinate = 338.25;
-  var paddle1Xspeed = 0;
-  var paddle2Xspeed = 0;
+  var paddle1 = paddle("#paddle1", -48, 160.125, 0, 10, 30)
+  var paddle2 = paddle("#paddle2", 338.25, 160.125, 0, 10, 30)
+  var ball1 = ball("#ball1", 160.125, 368.2875, 0, 0, 10, 10)
+  var ball2 = ball("#ball2", 160.125, -58.0375, 0, 0, 10, 10)
+
+
+  
   let interval;
 
   
@@ -54,6 +57,7 @@ function runProgram(){
   function newFrame() {
     repositionGameItem()
     redrawGameItem()
+    paddlesInBounds()
 
   }
   
@@ -68,33 +72,67 @@ function runProgram(){
 
   function handleKeyDown(event) {
     if (event.which === KEY.LEFTA) {
-      paddle1Xspeed = -5;
+      paddle1.xSpeed = -5;
     }
     if (event.which === KEY.RIGHTD) {
-      paddle1Xspeed = 5;
+      paddle1.xSpeed = 5;
     }
-    if (event.which === KEY.LEFT1) {
-      paddle2Xspeed = -5;
+    if (event.which === KEY.arrowLeft) {
+      paddle2.xSpeed = -5;
     }
-    if (event.which === KEY.RIGHT3) {
-      paddle2Xspeed = 5;
+    if (event.which === KEY.arrowRight) {
+      paddle2.xSpeed = 5;
     }
   }
 
   function handleKeyUp(event) {
     if (event.which === KEY.LEFTA) {
-      paddle1Xspeed = 0;
+      paddle1.xSpeed = 0;
     }
     if (event.which === KEY.RIGHTD) {
-      paddle1Xspeed = 0;
+      paddle1.xSpeed = 0;
     }
-    if (event.which === KEY.LEFT1) {
-      paddle2Xspeed = 0;
+    if (event.which === KEY.arrowLeft) {
+      paddle2.xSpeed = 0;
     }
-    if (event.which === KEY.RIGHT3) {
-      paddle2Xspeed = 0;
+    if (event.which === KEY.arrowRight) {
+      paddle2.xSpeed = 0;
     }
   }
+
+  function paddle(ID, x, y, xSpeed, height, width) {
+    return {
+      ID: ID,
+      x: x,
+      y: y,
+      xSpeed: xSpeed,
+      height: height,
+      width: width
+    }
+  }
+
+  function ball(ID, x, y, xSpeed, ySpeed, height, width) {
+    return {
+      ID: ID,
+      x: x,
+      y: y,
+      xSpeed: xSpeed,
+      ySpeed: ySpeed,
+      height: height, 
+      width: width,
+    }
+  }
+
+  function paddlesInBounds() {
+    if (paddle1.x >= -30 || paddle1.x <= -389.825) {
+      paddle1.x = paddle1.x - paddle1.xSpeed
+    }
+    if (paddle2.x <= 320.25 || paddle2.x >= 680.175) {
+      paddle2.x = paddle2.x - paddle2.xSpeed
+    }
+  }
+
+
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -110,14 +148,25 @@ function runProgram(){
   }
 
   function repositionGameItem() {
-    paddle1Xcoordinate += paddle1Xspeed
-    paddle2Xcoordinate += paddle2Xspeed
+    // updates paddles postion base on speed
+    paddle1.x += paddle1.xSpeed
+    paddle2.x += paddle2.xSpeed
+    // redraws balls base on speed
    }
  
    function redrawGameItem() {
-    $('.paddle1').css("left", paddle1Xcoordinate)
-    $('.paddle2').css("left", paddle2Xcoordinate)
+    //redraws paddles postions
+    $("#paddle1").css("left", paddle1.x)
+    $("#paddle2").css("left", paddle2.x)
+    $("#paddle1").css("top", paddle1.y)
+    $("#paddle2").css("top", paddle2.y)
+    // redraws balls postions
+    $("#ball1").css("left", ball1.x)
+    $("#ball2").css("left", ball2.x)
+    $("#ball1").css("top", ball1.y)
+    $("#ball2").css("top", ball2.y)
   }
+
 }
 
 
